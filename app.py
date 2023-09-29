59cafd895b6afff31ac8fc13dc0bf32b8bab5b52
Mysuwaphit <suwaphit.chotsawad@gmail.com>
2023-09-29 19:40:04 +0700

    Update code

--- app.py
+++ app.py
@@ -1,76 +1,39 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 3,
-   "id": "df16f18b-f321-4644-b5da-d61ea975d6e5",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import streamlit as st\n",
-    "import joblib\n",
-    "import pandas as pd \n",
-    "\n",
-    "# Load the trained model\n",
-    "model_filename = \"assignment04_model.pkl\"\n",
-    "pipeline = joblib.load(model_filename)\n",
-    "\n",
-    "# Create a Streamlit app\n",
-    "st.title(\"Regression Model App\")\n",
-    "st.sidebar.header('User Input')\n",
-    "\n",
-    "# Input fields for user to enter feature values\n",
-    "hours_studied = st.sidebar.slider('Hours Studied', 0, 10, 5)\n",
-    "previous_scores = st.sidebar.slider('Previous Scores', 0, 100, 50)\n",
-    "sleep_hours = st.sidebar.slider('Sleep Hours', 0, 12, 6)\n",
-    "\n",
-    "# Predict the target performance for the user inputs\n",
-    "user_input = np.array([hours_studied, previous_scores, sleep_hours]).reshape(1, -1)\n",
-    "prediction = model.predict(user_input)\n",
-    "\n",
-    "st.write('### Prediction')\n",
-    "st.write(f'Target Performance: {prediction[0]:.2f}')\n",
-    "\n",
-    "# # Predict target performance\n",
-    "# if st.sidebar.button(\"Predict\"):\n",
-    "#     features = {\n",
-    "#         \"Hours Studied\": hours_studied,\n",
-    "#         \"Previous Scores\": previous_scores,\n",
-    "#         \"Sleep Hours\": sleep_hours\n",
-    "#     }\n",
-    "#     input_data = pd.DataFrame(features, index=[0])\n",
-    "#     prediction = pipeline.predict(input_data)\n",
-    "#     st.write(f\"Predicted Target Performance: {prediction[0]:.2f}\")\n",
-    "    \n",
-    "    # st.write(f'Mean Absolute Error: {mae}')\n",
-    "    # st.write(f'Mean Squared Error: {mse}')\n",
-    "    # st.write(f'R-squared (R^2): {r2}')\n",
-    "\n",
-    "if __name__ == \"__main__\":\n",
-    "    # main()\n",
-    "    st.run()"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.11.5"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import streamlit as st
+import joblib
+import pandas as pd 
+
+
+# Load the trained model
+model_filename = "assignment04_model.pkl"
+model_and_metrics = joblib.load(model_filename)
+
+# Extract the model and metrics
+model = model_and_metrics['model']
+mae = model_and_metrics['mae']
+mse = model_and_metrics['mse']
+r2 = model_and_metrics['r2']
+
+# Create a Streamlit app
+st.title("Regression Model App")
+st.sidebar.header('User Input')
+
+# Input fields for user to enter feature values
+hours_studied = st.sidebar.slider('Hours Studied', 0, 10, 5)
+previous_scores = st.sidebar.slider('Previous Scores', 0, 100, 50)
+sleep_hours = st.sidebar.slider('Sleep Hours', 0, 12, 6)
+
+# Predict target performance
+if st.sidebar.button("Predict"):
+    features = {
+        "Hours Studied": hours_studied,
+        "Previous Scores": previous_scores,
+        "Sleep Hours": sleep_hours
+    }
+    input_data = pd.DataFrame(features, index=[0])
+    prediction = model.predict(input_data)
+    st.write('### Prediction')
+    st.write(f"Predicted Target Performance: {prediction[0]:.2f}")
+
+    st.write(f'Mean Absolute Error: {mae}')
+    st.write(f'Mean Squared Error: {mse}')
+    st.write(f'R-squared (R^2): {r2}')
